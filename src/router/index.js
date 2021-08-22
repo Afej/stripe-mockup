d883cee820293ba05466f4e27854fad0c225edc9
Afej <afekurojoshua@gmail.com>
2021-08-22 02:26:54 +0100

    Dynamic page titles

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,28 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/dashboard",
     name: "Dashboard",
     component: () => import("../views/Dashboard.vue"),
+    meta: {
+      title: "Dashboard",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
